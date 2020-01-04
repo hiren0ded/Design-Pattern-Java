5502f1c256d36993acbcd6451174c94e30020bb6
hiren0ded <15bce077@nirmauni.ac.in>
2020-01-04 17:10:08 +0530

    Added Test Class For Singleton Class

---
+++ src/test/java/SingletonTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import singleton.Singleton;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("Object Reference Check")
+    void singleInstance()
+    {
+        Singleton obj1 = Singleton.getInstance();
+        Singleton obj2 = Singleton.getInstance();
+        Singleton obj3 = Singleton.getInstance();
+
+        Assertions.assertSame(obj1, obj2);
+        Assertions.assertSame(obj2, obj3);
+    }
+
+}
